7d24ef96fbd2d62c691ae7e992f5adc9f9ef4a76
sasha <sasha.laigle2022@campus-eni.fr>
2023-03-07 09:46:46 +0100

    Intégration et affichage des différentes générations de pokemons

--- src/app/composants/generations/generations.component.ts
+++ src/app/composants/generations/generations.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-generations',
   templateUrl: './generations.component.html',
   styleUrls: ['./generations.component.css']
 })
-export class GenerationsComponent {
+export class GenerationsComponent implements OnInit {
 
+  generations = [
+    'Génération I',
+    'Génération II',
+    'Génération III',
+    'Génération IV',
+    'Génération V',
+    'Génération VI',
+    'Génération VII',
+  ];
+
+  constructor() {
+  }
+
+  ngOnInit(){
+
+  }
 }
