db4083d0148e8b1a2a2b9dd7ddcaa41d19bf6699
sasha <sasha.laigle2022@campus-eni.fr>
2023-03-09 12:12:27 +0100

    Ajout des pokemons component

---
+++ src/app/composants/pokemon/pokemon.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+// import { EventEmitter } from 'events';
+import {Pokemon} from "../../modeles/Pokemon";
+
+@Component({
+  selector: 'app-pokemon',
+  templateUrl: './pokemon.component.html',
+  styleUrls: ['./pokemon.component.css']
+})
+export class PokemonComponent implements OnInit {
+
+  public pokemonSelectionne: Pokemon | null;
+
+  @Input() public pokemon! : Pokemon;
+
+  @Output() evenementClicImage = new EventEmitter;
+
+  constructor() {
+    this.pokemonSelectionne = null;
+  }
+
+
+
+
+  ngOnInit(): void {
+  }
+
+  clicSurImage(nom: string){
+    this.evenementClicImage.emit(nom);
+  }
+
+
+}
